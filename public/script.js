13de7ae42d75d9fb2e28df652c381b6a472c123f
Your NameBChoubey <choubey.vasudha@gmail.com>
2020-05-25 12:26:53 +0530

    Code commited for Initial cart view

---
+++ public/script.js
@@ -0,0 +1,55 @@
+new Vue({
+	el: "#app",
+	data: {
+		total: 0,
+		products: [
+			{ title: "Product 1", id: 1, price: 9.99 },
+			{ title: "Product 2", id: 2, price: 9.99 },
+			{ title: "Product 3", id: 3, price: 9.99 }
+		],
+		cart: [],
+		search: ""
+	},
+	methods: {
+		addToCart: function (product) {
+			var found = false;
+			this.total += product.price;
+			for (var i = 0; i < this.cart.length; i++) {
+				if (this.cart[i].id === product.id) {
+					this.cart[i].qty++;
+					found = true;
+				}
+			}
+			if (!found) {
+				this.cart.push(
+					{
+						title: product.title,
+						id: product.id,
+						price: product.price,
+						qty: 1
+					}
+				);
+			}
+		},
+		inc: function (item) {
+			item.qty++;
+			this.total += item.price;
+		},
+		dec: function (item) {
+			item.qty--;
+			this.total -= item.price;
+			if (item.qty <= 0) {
+				var i = this.cart.indexOf(item);
+				this.cart.splice(i, 1);
+			}
+		},
+		onSubmit: function () {
+			console.log('pppppp');
+		}
+	},
+	filters: {
+		currency: function (price) {
+			return "$".concat(price.toFixed(2));
+		}
+	}
+})
